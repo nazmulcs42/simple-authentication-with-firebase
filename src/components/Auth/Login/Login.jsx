ee8556e89a0fc45a6e57fe36cad41e07c1250611
nazmulcs42 <nazmul.islam.lotif@gmail.com>
2023-05-02 10:04:13 +0600

    firebase login with useremail and password added

--- src/components/Auth/Login/Login.jsx
+++ src/components/Auth/Login/Login.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+import app from '../../../firebase.config';
+
+
+const auth = getAuth(app);
 
 const Login = () => {
+  const[user, setUser] = useState(null);
+  const [error, setError] = useState('');
+  const emailRef = useRef();
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then(res => {
+      const loggedUser = res.user;
+      console.log(loggedUser);
+      if(!loggedUser.emailVerified) {
+        alert("Please verify your email address first.");
+        return;
+      }
+      setUser(loggedUser);
+      setError('');
+      event.target.reset();
+    })
+    .catch(err => {
+      console.log(err.message);
+      setUser(null);
+      setError(err.message);
+    })
+  }
+
+  const handleForgotPassword = () => {
+    const resetEmail = emailRef.current.value;
+    if(!resetEmail) {
+      alert("Please provide an email address.");
+      return;
+    }
+    sendPasswordResetEmail(auth, resetEmail)
+    .then(res => {
+      alert("Please check your email address.");
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+  }
+
   return (
-    <div>Login</div>
+    <div className='w-50 m-5 p-3 border'>
+      <h3 className='my-3'>Sign In</h3>
+      { 
+        error && <div className='text-white bg-danger mb-3 p-3 rounded-1'> { error }</div> 
+      }
+      
+      <Form onSubmit={handleFormSubmit} className="my-3">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" name='email' ref={emailRef} placeholder="Your Email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" name="password" placeholder="Your Password" />
+      </Form.Group>
+      <Form.Group className="mb-3 terms-and-conditions" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Remember Me" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
+      <p><small>Forgot Password? <button className='border-0' onClick={handleForgotPassword}>Reset</button></small></p>
+    </div>
   )
 }
 
--- src/components/Auth/Register/Register.jsx
+++ src/components/Auth/Register/Register.jsx
@@ -1,49 +1,109 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import app from '../../../firebase.config';
 import './Register.css';
 
 const auth = getAuth(app);
 
 const Register = () => {
-  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+  const [success, setSeccess] = useState('');
 
   const handleFormSubmit = event => {
     event.preventDefault();
+    setSeccess('');
     const email = event.target.email.value;
     const password = event.target.password.value;
+    const userName = event.target.name.value;
+    //validate
+    if(!/(?=.*[A-Z])/.test(password)) {
+      setError("Please add at least one uppercase.");
+      return;
+    }
+    else if(!/(?=.*[0-9].*[0-9])/.test(password)) {
+      setError("Please add at least two numbers.");
+      return;
+    }
+    else if(password.length<6) {
+      setError("Please add at least 6 characters.");
+      return;
+    }
+
     createUserWithEmailAndPassword(auth, email, password)
     .then(res => {
       const loggedUser = res.user;
+      setUserName(loggedUser, userName);
       console.log(loggedUser);
-      setUser(loggedUser);
+      setError('');
+      setSeccess("Your account has been created successfully.");
+      event.target.reset();
+      verifyEmail(loggedUser);
     })
     .catch(err => {
       console.log(err.message);
+      setSeccess('');
+      setError(err.message);
     })
 
   }
-  return (
-    <div>
-      <h5>Sign-up for a new account</h5>
-      <Form onSubmit={handleFormSubmit} className="my-3">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name='email' placeholder="Your Email" />
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" placeholder="Your Password" />
-      </Form.Group>
-      <Form.Group className="mb-3 terms-and-conditions" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check Terms & Conditions." />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+  const setUserName = (loggedUser, userName) => {
+    updateProfile(loggedUser, {
+      displayName: userName
+    }).then(() => {
+      console.log("Profile updated")
+    }).catch((error) => {
+      console.log("An error occurred. ", error.message)
+    });
+    
+  }
+  const verifyEmail = (user) => {
+    sendEmailVerification(user)
+    .then(res => {
+      console.log(res);
+      alert("Please verify your email address.")
+    })
+    .catch(err =>{
+      console.log(err.message);
+    })
+  }
+
+  return (
+    <div className='w-50 m-5 p-3 border'>
+      <h5 className='my-3'>Sign-up for a new account</h5>
+      { 
+        error && <div className='text-white bg-danger mb-3 p-3 rounded-1'> { error }</div> 
+      }
+
+      { 
+        success && <div className='text-white bg-success mb-3 p-3 rounded-1'> { success } </div> 
+      }
+      
+      <Form onSubmit={handleFormSubmit} className="my-3">
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>User Name</Form.Label>
+          <Form.Control type="text" name='name' placeholder="User Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" name='email' placeholder="Your Email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" name="password" placeholder="Your Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-3 terms-and-conditions" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check Terms & Conditions." />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
     </Form>
 
     </div>
